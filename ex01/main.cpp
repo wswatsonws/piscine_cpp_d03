de0d248e45a8f9186f8aa559902ac637bd70723b
Watson <you@example.com>
2018-07-04 22:53:47 +0800

    my commit

---
+++ ex01/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "KoalaBot.h"
+#include "Parts.h"
+int main()
+{
+	/*
+	KoalaBot kb;
+
+ std::cout << std::boolalpha << kb.status() << std::endl;
+
+kb.informations();
+ 
+
+  Arms a1;
+  Head a2("coucou",false);
+  Legs a3("watson",true);
+  
+  kb.swapParts(a3);
+  std::cout << std::boolalpha << kb.status() << std::endl;
+  kb.informations();
+  return 0;*/
+ 
+ KoalaBot kb;
+
+ std::cout << std::boolalpha << kb.status() << std::endl;
+
+ kb.informations();
+
+ return 0;
+
+
+ }
--- ex02/KreogCom.h
+++ ex02/KreogCom.h
@@ -1,26 +1,19 @@
-#ifndef KREOGCOM_H_
-#define KREOGCOM_H_
-
+#ifndef KREOGCOM_H
+# define KREOGCOM_H
+#include <string>
 #include <iostream>
-
-class KreogCom
-{
-public:
-    KreogCom(int x, int y, int serial);
-    ~KreogCom();
-
-    void addCom(int x, int y, int serial);
-    KreogCom* getCom();
-    void removeCom();
-
-    void ping() const;
-    void locateSquad() const;
-
-private:
-    KreogCom* _next;
-    int _x;
-    int _y;
-    const int m_serial;
+class KreogCom{
+  const int _serial;
+  KreogCom *_link;
+  int _x, _y;
+ public:
+  KreogCom(int x, int y, int serial);
+  ~KreogCom();
+  void addCom(int x, int y, int serial);
+  KreogCom *getCom();
+  void removeCom();
+  void ping() const;
+  void locateSquad() const;
 };
 
 #endif
