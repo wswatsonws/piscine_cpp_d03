b2091a6ee5f74c9917809b72b3f79228fe5efb30
Watson <you@example.com>
2018-07-05 08:34:46 +0800

    my commit

--- ex01/Parts.cpp
+++ ex01/Parts.cpp
@@ -29,12 +29,12 @@ Legs::~Legs(){
 
 void Legs::informations() const{
     if (this->_func)
-		std::cout << "\t[Parts] Legs " << this->_serial << " status : OK" <<std::endl;
-	else
 		std::cout << "\t[Parts] Legs " << this->_serial << " status : KO" <<std::endl;
+	else
+		std::cout << "\t[Parts] Legs " << this->_serial << " status : OK" <<std::endl;
 }
 
-//
+
 
 Head::Head(std::string serial, bool f){
   _serial = serial;
